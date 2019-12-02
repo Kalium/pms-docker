90d13ce41cf65cdcef98f21a560d67ccce635977
jwagnerk <kalium@gmail.com>
2019-12-01 20:36:06 -0800

    Haaaaaaaaack

--- root/installBinary.sh
+++ root/installBinary.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-. /plex-common.sh
+curl -J -L -o /tmp/plexmediaserver.deb https://downloads.plex.tv/plex-media-server-new/1.18.2.2058-e67a4e892/debian/plexmediaserver_1.18.2.2058-e67a4e892_arm64.deb
 
-echo "${TAG}" > /version.txt
-if [ ! -z "${URL}" ]; then
-  echo "Attempting to install from URL: ${URL}"
-  installFromRawUrl "${URL}"
-elif [ "${TAG}" != "beta" ] && [ "${TAG}" != "public" ]; then
-  getVersionInfo "${TAG}" "" remoteVersion remoteFile
-
-  if [ -z "${remoteVersion}" ] || [ -z "${remoteFile}" ]; then
-    echo "Could not get install version"
-    exit 1
-  fi
-  
-  echo "Attempting to install: ${remoteVersion}"
-  installFromUrl "${remoteFile}"
-fi
+dpkg -i --force-confold /tmp/plexmediaserver.deb
+rm -f /tmp/plexmediaserver.deb
